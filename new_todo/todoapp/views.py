a6e0b3e3bc7b8e3460685a3a3f9856070a330ff2
Messi002 <rostandmessi2@gmail.com>
2024-05-16 14:30:00 +0100

    added forms to views

--- new_todo/todoapp/views.py
+++ new_todo/todoapp/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 def task_list(request):
-    context = {}
+    tasks = Task.objects.all()
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            redirect("task-list") #re-directing to the task view by using the name
+    context = {"tasks":tasks}
     return render(request, "task_list.html", context)
 
 def task_update(request):
