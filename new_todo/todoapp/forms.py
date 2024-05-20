b69e22b89f01149e0fb4a8dc4623f4838705c43c
Messi002 <rostandmessi2@gmail.com>
2024-05-20 11:19:41 +0100

    added views, templates

--- new_todo/todoapp/forms.py
+++ new_todo/todoapp/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Task
 
 class TaskForm(forms.ModelForm):
+
+    title = forms.CharField(widget=forms.TextInput(attrs={"class":"myinput", "placeholder":"Enter Todo Messi"}))
     class Meta:
         model = Task
         fields = ["title"]
--- new_todo/todoapp/views.py
+++ new_todo/todoapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 
@@ -10,12 +10,21 @@ def task_list(request):
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-            redirect("task-list") #re-directing to the task view by using the name
-    context = {"tasks":tasks}
+            return redirect("task-list") #re-directing to the task view by using the name
+    context = {"tasks":tasks, "form" : form}
     return render(request, "task_list.html", context)
 
-def task_update(request):
-    context = {}
+def task_update(request, pk):
+    task = Task.objects.get(id = pk)
+    form = TaskForm(instance = task)
+    if request.method == 'POST':
+        form = TaskForm(request.POST,instance = task)
+        if form.is_valid():
+            form.save()
+            return redirect("task-list") #re-directing to the task view by using the name
+
+    
+    context = {"form":form}
     return render(request, "task_list.html", context)
 
 def task_delete(request):
